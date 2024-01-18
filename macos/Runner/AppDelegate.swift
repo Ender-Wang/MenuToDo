bde3c494989bf624963c60c3a7451ff005dd8ad8
Rui Wang <ge45dey@mytum.de>
2024-01-19 00:07:55 +0100

    chore(dev): settings for dev only

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,25 +3,30 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-    var statusBar: StatusBarController?
-    var popover = NSPopover.init()
-    override init() {
-        popover.behavior = NSPopover.Behavior.transient //to make the popover hide when the user clicks outside of it
-    }
-    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return false
-    }
-    override func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let controller: FlutterViewController =
-        mainFlutterWindow?.contentViewController as! FlutterViewController
-        popover.contentSize = NSSize(width: 320, height: 510) //change this to your desired size
-        popover.contentViewController = controller //set the content view controller for the popover to flutter view controller
-        statusBar = StatusBarController.init(popover)
-        guard let window = mainFlutterWindow else {
-            print("mainFlutterWindow is nil")
-            return
-        }
-        window.close() //close the default flutter window
-        super.applicationDidFinishLaunching(aNotification)
-    }
+  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+
+  // TODO on Release: comment all lines above, uncomment all below
+  // var statusBar: StatusBarController?
+  // var popover = NSPopover.init()
+  // override init() {
+  //     popover.behavior = NSPopover.Behavior.transient //to make the popover hide when the user clicks outside of it
+  // }
+  // override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+  //     return false
+  // }
+  // override func applicationDidFinishLaunching(_ aNotification: Notification) {
+  //     let controller: FlutterViewController =
+  //     mainFlutterWindow?.contentViewController as! FlutterViewController
+  //     popover.contentSize = NSSize(width: 320, height: 510) //change this to your desired size
+  //     popover.contentViewController = controller //set the content view controller for the popover to flutter view controller
+  //     statusBar = StatusBarController.init(popover)
+  //     guard let window = mainFlutterWindow else {
+  //         print("mainFlutterWindow is nil")
+  //         return
+  //     }
+  //     window.close() //close the default flutter window
+  //     super.applicationDidFinishLaunching(aNotification)
+  // }
 }
--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,12 +4,31 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
+    // Set window size
+    let windowFrame = NSRect(x: 0, y: 0, width: 320, height: 510)
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+
+    // Make the window non-resizable
+    self.styleMask.remove(.resizable)
+
+    // // Set the transparency of app window, disclaimer: this makes everything transparent, not just the bg
+    // self.isOpaque = false
+    // self.backgroundColor = NSColor.clear
+    // self.alphaValue = 0.5
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
+
+    // TODO on Release: comment all lines above, uncomment all below
+    // let flutterViewController = FlutterViewController()
+    // let windowFrame = self.frame
+    // self.contentViewController = flutterViewController
+    // self.setFrame(windowFrame, display: true)
+
+    // RegisterGeneratedPlugins(registry: flutterViewController)
+
+    // super.awakeFromNib()
   }
 }
