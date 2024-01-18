d17ceb615830befb969452241e7e2b27c9d8a8fb
Rui Wang <ge45dey@mytum.de>
2024-01-19 00:08:54 +0100

    feat(LoginPage): add login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  // TODO: http request on authrization, and localhost callback monitering to get the 'access_token'
+  void login() async {
+    /**
+    'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
+            '?client_id=YOUR_CLIENT_ID'
+            '&response_type=code'
+            '&redirect_uri=YOUR_REDIRECT_URI'
+            '&scope=openid+offline_access+User.Read'
+    */
+    final loginURL = Uri.parse('https://www.google.com');
+
+    if (await canLaunchUrl(loginURL)) {
+      await launchUrl(loginURL);
+    } else {
+      throw 'Could not launch $loginURL';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // Logo
+              Image.asset(
+                'lib/images/launch_logo.png',
+              ),
+
+              //Slogan
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 18),
+                child: Text(
+                  'Todo app, but\n only in the Menubar.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              // Login button
+              TextButton(
+                onPressed: login,
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey.shade600,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14))),
+                child: IntrinsicWidth(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Login with ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Image.asset(
+                        'lib/images/icons8-microsoft-48.png',
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
